567a051f73e9f8a864bfaf0b0623a0f92c76cd52
nathanramsey <nathanramsey@firmstep.com>
2017-11-07 15:43:58 +0000

    changed file name and structure

---
+++ medier/App.js
@@ -0,0 +1,13 @@
+//import libararies for making component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+//make a component
+const App = () => (
+  <Header />
+);
+
+
+//make the component available for other parts of the app
+export default App;
