3b997269b3eca09f00cf98600064c5da2852eea4
nathanramsey <nathanramsey@firmstep.com>
2018-01-29 16:36:16 +0000

    just practicing

--- medier/App.js
+++ medier/App.js
@@ -1,18 +1,83 @@
-import React, { Component } from 'react';
-import { Image, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 
-export default class App extends Component {
-  render() {
+
+//create stuff
+class App extends React.Component{
+  state = {
+      todo: "",
+      todo: []
+  }
+  addTodo = () =>{
+      var newTodo = this.state.text;
+      var arr = this.state.todo;
+      arr.push(newTodo);
+      this.setState({todo: arr, text: ""});
+  }
+  deleteTodo = (t) =>{
+    var arr = this.state.todo;
+    var pos = arr.indexOf(t);
+    arr.splice(pos,1);
+    this.setState({todo: arr});
+  }
+  renderTodos = () =>{
+    return this.state.todo.map(t=>{
+      return (
+        <Text
+        key={t}
+        onPress={()=>{this.deleteTodo(t)}}
+        >{t}</Text>
+      )
+    })
+  }
+  render(){
     return (
-      <Image source={{uri: 'http://i.imgur.com/IGlBYaC.jpg'}} style={s.backgroundImage} />
-    );
+      <View>
+      <View style={styles.viewStyle}>
+        <Text style={styles.header}> Add a new task</Text>
+        <TextInput
+          style={styles.inputStyle}
+          onChangeText={(text)=>this.setState({text})}
+          value={this.state.text }
+        />
+        <Button
+          title="Add todo"
+          color="green"
+          onPress={this.addTodo}
+        />
+        {this.renderTodos()}
+      </View>
+      </View>
+    )
   }
 }
 
-const s = StyleSheet.create({
-  backgroundImage: {
-      flex: 1,
-      width: null,
-      height: null,
-  }
-});
+const styles = {
+  viewStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 100,
+    backgroundColor: "#FFEB3B"
+  },
+    inputStyle:{
+      height: 40,
+      width: 300,
+      borderColor: "grey",
+      color: "black",
+      padding: 10,
+      borderWidth: 2,
+      backgroundColor: "yellow",
+      borderRadius: 5,
+      margin: 10
+    },
+    container: {
+      flex: 1
+    },
+    header: {
+      fontSize: 20,
+      color: 'green',
+      fontWeight: 'bold'
+    }
+}
+
+export default App;
